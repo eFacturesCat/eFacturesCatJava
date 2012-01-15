262ec05f5c74fc8a9d123af72f78f23773321cfc
santicasas <santicasas@6cc403f9-ccea-43c0-be1b-916b681f2f23>
2012-01-15 17:12:35 +0000

    Partial implementation of secure module

---
+++ trunk/api/common/src/main/java/cat/eFactures/common/Utils.java
@@ -0,0 +1,46 @@
+package cat.eFactures.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class Utils {
+	  /**
+	   * Read XML as DOM.
+	   */
+	  public static Document readXml(InputStream is) throws SAXException, IOException,
+	      ParserConfigurationException {
+	      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+	      dbf.setValidating(false);
+	      dbf.setIgnoringComments(false);
+	      dbf.setIgnoringElementContentWhitespace(true);
+	      dbf.setNamespaceAware(true);
+	      // dbf.setCoalescing(true);
+	      // dbf.setExpandEntityReferences(true);
+
+	      DocumentBuilder db = null;
+	      db = dbf.newDocumentBuilder();
+	      db.setEntityResolver(new NullResolver());
+
+	      // db.setErrorHandler( new MyErrorHandler());
+
+	      return db.parse(is);
+	  }
+	}
+
+	class NullResolver implements EntityResolver {
+	  public InputSource resolveEntity(String publicId, String systemId) throws SAXException,
+	      IOException {
+	    return new InputSource(new StringReader(""));
+	  }
+	}
--- trunk/api/transform/src/main/java/cat/eFactures/transform/Facturae_3_2.java
+++ trunk/api/transform/src/main/java/cat/eFactures/transform/Facturae_3_2.java
@@ -1,5 +1,12 @@
 package cat.eFactures.transform;
 
+import java.io.FileNotFoundException;
+
 public final class Facturae_3_2 extends XMLInvoice {
+
+	public Facturae_3_2(String fileName) throws FileNotFoundException {
+		super(fileName);
+		// TODO Auto-generated constructor stub
+	}
 
 }
